cf0f97c4e947491c756e68c2ef6f30fd831ca28b
Ivan Zhilnikov <35070562+zhiliv@users.noreply.github.com>
2020-05-07 22:10:55 +0300

    Яистый проект

--- app/renderer/renderer.js
+++
@@ -1,9 +0,0 @@
-//Подключение модуль для работы с Electron
-const { remote } = require('electron');
-//Стартовый файл компилированными скриптами
-import './index.html';
-//начальный компонент
-import AppRoot from './view/index';
-window.i18next = remote.getGlobal('i18next');
-//рендер компонента
-AppRoot.renderSync({}).appendTo(document.body);
